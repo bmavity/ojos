af14c1cd1ddacec51d99f756b226937387c63447
Brian Mavity <brian@brianmavity.com>
2011-04-18 17:36:09 -0500

    Added directory structure

---
+++ auto.js
@@ -0,0 +1,43 @@
+var fs = require('fs')
+    resourceDir = __dirname + '/resources/',
+    resources = {};
+
+var findSomethings = function autoFindSomethings(rType) {
+  Object.keys(resources).forEach(function(resourceName) {
+    var resource = resources[resourceName];
+    forEachDirectory(resource.path + rType + '/', function(dirname) {
+      resource[rType][dirname] = {
+        path: resource.path + '/' + dirname
+      };
+    });
+  });
+};
+
+var forEachDirectory = function autoforEachDirectory(dirname, operation) {
+  fs.readdirSync(dirname).filter(isDirectory(dirname)).forEach(operation);
+};
+
+var findResources = function autoFindResources() {
+  forEachDirectory(resourceDir, function(dirname) {
+    resources[dirname] = {
+      actions: {},
+      path: getPath(dirname),
+      views: {}
+    };
+  });
+};
+
+var getPath = function autoGetPath(name) {
+  return resourceDir + name + '/';
+};
+
+var isDirectory = function(dirname) {
+  return function(name) {
+    return fs.statSync(dirname + name).isDirectory();
+  };
+};
+
+findResources();
+findSomethings('views');
+findSomethings('actions');
+console.log(resources);
